44c58e4bb97c0b35cf28bb41f58c06588b022719
facundoalves1 <facundoalves1@gmail.com>
2023-06-26 12:12:47 -0300

    Data Base Connection

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const {dbConnection} = require('./db/dbConnection');
 
 //Middlewares
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 dotenv.config({path: '.env'});
+
+//DBCONNECTION
+dbConnection();
 
 //Server
 const PORT = process.env.PORT || 3001;
