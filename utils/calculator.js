54724e18ff49b689dc5b2b3b0f9ec6d9494b6767
facundoalves1 <facundoalves1@gmail.com>
2023-10-24 06:20:30 -0300

    Added quantity functionality

--- utils/calculator.js
+++ utils/calculator.js
@@ -2,8 +2,9 @@ const calculator = (req) => {
   const { items } = req.body;
   
   const result = items.reduce((acc, current) => {
-    console.log(current.price)
+    
     return acc + current.price * current.quantity;
+    
   }, 0);
   
   return result;
